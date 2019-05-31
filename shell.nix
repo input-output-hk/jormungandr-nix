d57b35537405cdfb12c7de1048fe0be1ea941a15
Samuel Leathers <samuel.leathers@iohk.io>
2019-05-30 23:00:54 -0400

    move everything to not be committed to secrets dir

--- shell.nix
+++ shell.nix
@@ -17,29 +17,29 @@ in pkgs.stdenv.mkDerivation {
     Create Wallet Delegation:
     TBD
     Create Staking Pool KES Keys and Stake Pool Certificate:
-    jcli key generate --type=Ed25519Extended | tee stake.key | jcli key to-public > stake.pub
-    jcli key generate --type=Curve25519_2HashDH | tee stake_pool_vrf.key | jcli key to-public > stake_pool_vrf.pub
-    jcli key generate --type=SumEd25519_12 | tee stake_pool_kes.key | jcli key to-public > stake_pool_kes.pub
+    jcli key generate --type=Ed25519Extended | tee secrets/stake.key | jcli key to-public > secrets/stake.pub
+    jcli key generate --type=Curve25519_2HashDH | tee secrets/stake_pool_vrf.key | jcli key to-public > secrets/stake_pool_vrf.pub
+    jcli key generate --type=SumEd25519_12 | tee secrets/stake_pool_kes.key | jcli key to-public > secrets/stake_pool_kes.pub
     jcli certificate new stake-pool-registration \
-          --kes-key $(cat stake_pool_kes.pub) \
-          --vrf-key $(cat stake_pool_vrf.pub) \
-          --serial 1010101010 > stake_pool.cert
-    cat stake_pool.cert | jcli certificate sign stake.key | tee stake_pool.cert
-    cat stake_pool.cert | jcli certificate get-stake-pool-id | tee stake_pool.id
+          --kes-key $(cat secrets/stake_pool_kes.pub) \
+          --vrf-key $(cat secrets/stake_pool_vrf.pub) \
+          --serial 1010101010 > secrets/stake_pool.cert
+    cat secrets/stake_pool.cert | jcli certificate sign secrets/stake.key | tee secrets/stake_pool.cert
+    cat secrets/stake_pool.cert | jcli certificate get-stake-pool-id | tee secrets/stake_pool.id
     Create Single address in staking key (for funds):
-    jcli address single $(cat stake.key) > stake.address
+    jcli address single $(cat secrets/stake.key) > secrets/stake.address
     Generate genesis and edit with initial certs and stake:
-    jcli genesis init > genesis.yaml
+    jcli genesis init > secrets/genesis.yaml
     Encode genesis block:
-    jcli genesis encode --input genesis.yaml --output block-0.bin
+    jcli genesis encode --input secrets/genesis.yaml --output secrets/block-0.bin
     Create node config:
     TBD
     Create secret:
     TBD
     Start jormungandr:
-    jormungandr --genesis-block block-0.bin \
-    --config config.yaml \
-    --secret node_secret.yaml
+    jormungandr --genesis-block secrets/block-0.bin \
+                --config config.yaml \
+                --secret secrets/secret.yaml
   EOF
   '';
 }
