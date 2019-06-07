30a8639f273599f9dcfcb700d1cde07e7ea1f67a
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2019-06-07 09:57:10 +0200

    default to shell.nix

--- default.nix
+++ default.nix
@@ -1,11 +1,1 @@
-{...}@args:
-# Imports the iohk-nix library.
-# The version can be overridden for debugging purposes by setting
-# NIX_PATH=iohk_nix=/path/to/iohk-nix
-with import ./lib.nix;
-let
-  setupStakePool = rustPkgs.callPackage ./nix/setup-stake-pool.nix args;
-in
-{
-  inherit iohkNix jormungandr setupStakePool block0 config;
-}
+import ./shell.nix
