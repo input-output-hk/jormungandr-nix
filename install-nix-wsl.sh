b04706991fdbb7d84e581b96766398da9c6696a6
Samuel Leathers <samuel.leathers@iohk.io>
2019-06-11 12:27:28 -0400

    add hydra binary cache

--- install-nix-wsl.sh
+++ install-nix-wsl.sh
@@ -5,6 +5,9 @@ sudo mkdir -p /etc/nix
 cat <<EOF | sudo tee /etc/nix/nix.conf
 sandbox = false
 use-sqlite-wal = false
+substituters = https://cache.nixos.org https://hydra.iohk.io
+trusted-substituters =
+trusted-public-keys = hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
 EOF
 
 curl https://nixos.org/nix/install | sh
