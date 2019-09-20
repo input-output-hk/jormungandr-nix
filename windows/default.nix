be133d8a0dce5273220cc270e30d6b7638858b22
Michael Fellinger <michael.fellinger@iohk.io>
2019-09-20 15:27:44 +0200

    jormungandr: upgrade to 0.3.9999

--- windows/default.nix
+++ windows/default.nix
@@ -18,6 +18,7 @@ let
     else (fetchurl {
       inherit url;
       sha256 = {
+       "0.3.999" = "0cnplldr827rf020s7qi9w215cidcgvkc6hpfqdxfxankxvffc6k";
        "0.3.3" = "1109lmh4d5k7xaqpkh7v3dw0z9jhqzbwmkd80l0c0sxqj2l3n7qn";
        "0.3.2" = "0yw6g02y9vdw7k5jmcmjbgwk63dicqkjm6lrawb40c6gvps1hmsf";
        "0.3.1" = "1b2k7g509diaqvir8rigb92amjiw80y60p2k439rmd2la5n29cji";
