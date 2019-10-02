718bc9aca4e46e2ce7920acab02829edacef140b
Samuel Leathers <samuel.leathers@iohk.io>
2019-10-02 11:05:37 -0400

    script improvements and set relative state dir

--- nix/shells/bootstrap.nix
+++ nix/shells/bootstrap.nix
@@ -8,7 +8,7 @@ with import ../../lib.nix; with lib;
 , faucetAmounts ? (map (i: faucetAmount) (lib.range 1 numberOfStakePools))
 , numberOfStakePools ? if (block0_consensus == "bft") then 0 else 3
 , numberOfLeaders ? if (numberOfStakePools == 0) then 1 else numberOfStakePools
-, rootDir ? "/tmp"
+, rootDir ? "./state-jormungandr"
 # need to declare other make-genesis.nix parameters to be able to pass them:
 , block0_date ? null
 , isProduction ? null
