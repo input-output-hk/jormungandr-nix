14653fdd457066fb7cfb7db7b24f9323ac70ee33
John Lotoski <john.lotoski@iohk.io>
2020-02-21 18:56:06 -0500

    Adds systemd python module for journald logging

--- nixos/jormungandr-monitor/default.nix
+++ nixos/jormungandr-monitor/default.nix
@@ -1,7 +1,7 @@
 { python3, makeWrapper, runCommand, jormungandr-cli, lsof, coreutils, gnugrep, iproute }:
 
 let
-  python = python3.withPackages (ps: with ps; [ prometheus_client dateutil ]);
+  python = python3.withPackages (ps: with ps; [ prometheus_client dateutil systemd ]);
 in runCommand "jormungandr-monitor" {
   buildInputs = [ python makeWrapper ];
   jcli = "${jormungandr-cli}/bin/jcli";
